f5ad74a4004f42468eaee9227f710bf883fe3774
Lucas Rezende <lucasffrezende@gmail.com>
2023-10-10 14:36:37 -0300

    Criação da estrutura do projeto

---
+++ src/main/java/br/edu/unifio/sistemabiomedicina/SistemaBiomedicinaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unifio.sistemabiomedicina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBiomedicinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaBiomedicinaApplication.class, args);
+    }
+
+}
