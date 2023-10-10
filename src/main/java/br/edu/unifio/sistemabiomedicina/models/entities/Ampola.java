b51d2f1ec0fe3a3619c703443d1cf16185c864f1
Lucas Rezende <lucasffrezende@gmail.com>
2023-10-10 15:15:32 -0300

    Entidade Ampola criada

---
+++ src/main/java/br/edu/unifio/sistemabiomedicina/models/entities/Ampola.java
@@ -0,0 +1,28 @@
+package br.edu.unifio.sistemabiomedicina.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_ampola")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ampola {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Long codigoInternacao;
+    private Integer ampolaMl;
+    private LocalDate dataCadastro;
+    private LocalDate dataValidade;
+
+    @ManyToOne
+    private Paciete paciete;
+
+}
